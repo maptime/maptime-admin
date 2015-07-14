c620f47d0108d3c73188f80bf06c98b8252d4afa
geografa <rafa@mapbox.com>
2015-07-14 08:18:02 -0700

    adds city to onbaord script. adds gitignore.

---
+++ onboard.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# ------------------------------------------------------------------------------------------------------ 
+# onboarding - Maptime onboarding tool
+# 
+# Bash script to assist in the Maptime onboarding process on Github.
+# Before running, user must have a Github personal access token
+# saved in an environment variable named GH_TOKEN
+# Requires jq for json parsing.  See: http://stedolan.github.io/jq/download/ 
+
+echo -n "Enter the Maptime chapter repo name (e.g. portland. Make sure it doesn't conflict with existing repos):"
+read chapter
+
+echo -n "Enter the abbreviations for country and/or state/province (e.g. us-ny for USA, New York): "
+read city
+
+echo -n "Enter the admin GitHub username: "
+read admin
+
+curl -H "Content-Type: application/json" \
+	-u ${GH_TOKEN}:x-oauth-basic https://api.github.com/orgs/maptime/repos \
+	-X POST -d "{\"name\":\"$chapter\",\"description\":\"Repo for Maptime $chapter\"}"
+
+tempdir=$(mktemp -dt "starter.XXXXXXXXXX")
+git clone git@github.com:maptime/starter.git $tempdir
+cd $tempdir
+git remote add local git@github.com:maptime/${chapter}.git
+git push local gh-pages
+
+id=$(curl -H "Content-Type: application/json" \
+	-u ${GH_TOKEN}:x-oauth-basic https://api.github.com/orgs/maptime/teams \
+	-d "{\"name\":\"${city}-admin\",\"permission\":\"write\",\"repo_names\":[\"maptime/${chapter}\"]}" \
+	| jq -r '.id')
+
+curl -u ${GH_TOKEN}:x-oauth-basic -X PUT https://api.github.com/teams/${id}/memberships/${admin}
